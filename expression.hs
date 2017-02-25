63095d31e8b48e78af3b796eff8454d5b0b2efc1
jarra <suhrawardi@gmail.com>
2017-02-25 10:47:49 +0100

    We now do a bit of IO

--- expression.hs
+++ expression.hs
@@ -3,5 +3,15 @@
 -- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
 --
 
+actionList = [putStr2 "Hello World\n",
+              writeFile "testfile.txt" "Howdy File system",
+              putStr2 "File written!\n"]
+
+putCharList :: String -> [IO()]
+putCharList = map putChar
+
+putStr2 :: String -> IO()
+putStr2 s = sequence_ (putCharList s)
+
 main :: IO ()
-main = putStr "Hello WOrld\n"
+main = sequence_ actionList
