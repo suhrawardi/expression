c3bd122c87bc414b4ce171066f2dd1c8a87a712b
jarra <suhrawardi@gmail.com>
2017-03-01 20:26:34 +0100

    Now we draw some shapes

---
+++ Main.hs
@@ -0,0 +1,11 @@
+--
+-- Copyright (c) 2017 Jarra <suhrawardi@gmail.com>
+-- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
+--
+
+module Main where
+-- import Sierpinski
+import Draw
+
+main :: IO ()
+main = draw
